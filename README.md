9e572692c5cc3e9602ee286f0ac3063d4b9b25e1
Paul Miller <miller_p@hotmail.fr>
2020-07-07 11:23:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GoogleTrend
+Module to make use of pytrends, which retrieves trends from the Google Trends API.
---
+++ trends.py
@@ -0,0 +1,72 @@
+"""
+Module to get google trends using pytrends.
+"""
+
+import pandas as pd
+from pytrends.request import TrendReq
+
+
+def by_region(kw_list, draw=True, drop_0=True, **kwargs):
+    """
+    Fetches the DataFrame of the Google Trend for a list of keywords. By
+    default, it will drop 0 values and will draw the plot.
+
+    Parameters
+    ----------
+    kw_list : list
+        List of keywords to search.
+    draw : bool, optional
+        Draw the bar plot for the keywords or not. The default is True.
+    drop_0 : bool, optional
+        Drop empty rows. The default is True.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing the keywords as columns and the country as index.
+
+    """
+    pytrend = TrendReq()
+    
+    pytrend.build_payload(kw_list=kw_list, **kwargs)
+    
+    df = pytrend.interest_by_region()
+    
+    if drop_0:
+        df.loc[(df!=0).any(axis=1)].plot(kind='bar')
+        return df.loc[(df!=0).any(axis=1)]
+    else:
+        df.plot(kind='bar')
+        return df
+
+
+def over_time(kw_list, draw=True, **kwargs):
+    """
+    Fetches the DataFrame of the Google Trend for a list of keywords. By
+    default, it will draw the plot.
+
+    Parameters
+    ----------
+    kw_list : list
+        List of keywords to search.
+    draw : bool, optional
+        Draw the bar plot for the keywords or not. The default is True.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing the keywords as columns and the country as index.
+
+    """
+    pytrend = TrendReq()
+    
+    pytrend.build_payload(kw_list=kw_list, **kwargs)
+    
+    df = pytrend.interest_over_time()
+    
+    df = df.drop('isPartial', axis=1)
+    df.plot()
+    
+    return df
+
+
